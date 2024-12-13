127216fb55bdaee8e1aa436db5b9e1d12cac8447
Your Name <soni.hiren108@gmail.com>
2024-12-13 14:03:46 +0200

    Bug fix

--- app/Components/ChatSim.tsx
+++ app/Components/ChatSim.tsx
@@ -1,7 +1,6 @@
 'use client'
 
 import React, { useState, useEffect, useRef } from 'react'
-import { Button } from "@/components/ui/button"
 import Image from 'next/image'
 
 import DwightLogo from '../../public/Dwight_Logo-Mark.jpeg'
