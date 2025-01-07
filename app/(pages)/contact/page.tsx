750bf79dec4b8c5e5f2756f0d45286ffe3ba413a
Your Name <soni.hiren108@gmail.com>
2025-01-07 11:23:53 +0200

    Alignment fix

--- app/(pages)/contact/page.tsx
+++ app/(pages)/contact/page.tsx
@@ -45,13 +45,13 @@ export default function BookDemo() {
                             </h2>
                             <div className="space-y-4 mb-8">
                                 <div className="flex items-center space-x-2">
-                                    <a href="mailto:info@justaskdwight.com">
+                                    <a href="mailto:info@justaskdwight.com" className='flex'>
                                         <Mail className="w-8 h-8 text-[#FFCB1F]" />
                                         <span className="text-white">info@justaskdwight.com</span>
                                     </a>
                                 </div>
                                 <div className="flex items-center space-x-2">
-                                    <a href="tel:+27604548361">
+                                    <a href="tel:+27604548361" className='flex'>
                                         <Phone className="w-8 h-8 text-[#FFCB1F]" />
                                         <span className="text-white">+27 60 454 8361</span>
                                     </a>
